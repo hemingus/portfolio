6428225ed91752fbd223abdf849e224e2d245a76
Heming Hanevik <Heming.Hanevik@student.uib.no>
2023-11-22 10:28:26 +0100

    🔧➕🔒 Secure ContactForm functionality

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,69 +1,80 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react'
 import './ContactForm.css'
+import emailjs from '@emailjs/browser'
 
 interface FormState {
-  name: string;
-  email: string;
-  message: string;
+    name: string;
+    email: string;
+    message: string;
 }
 
 const ContactForm: React.FC = () => {
-  const [formData, setFormData] = useState<FormState>({
-    name: '',
-    email: '',
-    message: ''
-  });
+    
+    const EMAILJS_SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
+    const EMAILJS_TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
+    const EMAILJS_PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
+    const [formData, setFormData] = useState<FormState>({
+        name: '',
+        email: '',
+        message: ''
+    })
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Process form data (you can send it to an API, log it, etc.)
-    console.log(formData);
-    // You can add your logic to send the form data to a server here
-    // For example: Send a POST request using fetch or axios
-  };
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target
+        setFormData(prevState => ({
+        ...prevState,
+        [name]: value
+        }))
+    }
 
-  return (
-    <div className="form-container">
-        <form onSubmit={handleSubmit}>
-            <label>
-                Name:
-                <input
-                type="text"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                />
-            </label>
-            <label>
-                Email:
-                <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                />
-            </label>
-            <label>
-                Message:
-            </label>
-                <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                />
-            
-            <button type="submit">Submit</button>
-        </form>
-    </div>
-  );
-};
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, e.currentTarget, EMAILJS_PUBLIC_KEY)
+        .then((result) => {
+            console.log(result.text)
+            alert("Email sent successfully!")
+        }, (error) => {
+            console.log("Error: Sending failed", error.text)
+            alert("Error: Sending failed.")
+        })
+        console.log(formData)
+    }
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Name:
+                    <input
+                    type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Email:
+                    <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Message:
+                </label>
+                    <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    />
+                
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
 
 export default ContactForm;
