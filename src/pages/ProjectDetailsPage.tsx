d50c1416cf8682cfcc7e61f1ebb5db871172f40d
Heming Hanevik <heming.hanevik@gmail.com>
2025-02-26 00:31:16 +0100

    Updated ProjectDetailsPage with projects-context

--- src/pages/ProjectDetailsPage.tsx
+++ src/pages/ProjectDetailsPage.tsx
@@ -1,12 +1,26 @@
 import { useParams } from 'react-router-dom'
+import { useProjects } from '../components/Context/ProjectsContext'
 
 const ProjectDetailsPage = () => {
-    const { projectName } = useParams<{projectName: string}>()
+    const { projectName } = useParams<{ projectName: string }>()
+    const projects = useProjects();
+  
+    const project = projects.find((p) => p.header.toLowerCase().replace(/\s+/g, "-") === projectName)
+    const path = '/galleryimages/'
+  
+    if (!project) {
+        return <h1>Project Not Found</h1>
+    }
+  
     return (
         <div>
-            <h1>Project: {projectName}</h1>
+            <h1>{project.header}</h1>
+            <img src={path + project.images[0]} alt={project.header} />
+            <p>{project.text}</p>
+            <p>{project.techstack}</p>
         </div>
-    )
+    );
+
 }
 
 export default ProjectDetailsPage
